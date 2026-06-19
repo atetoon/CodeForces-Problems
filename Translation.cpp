6a1c266daa49ec9be7fd3f7bb3e16cfdf6fa8fc9
atetoon <i.am.dhruvpandey@gmail.com>
2026-06-19 20:56:03 +0530

    made changes to Translation problems's solution

--- Translation.cpp
+++ Translation.cpp
@@ -4,6 +4,5 @@ using namespace std;
 int main() {
     string a, b; cin >> a >> b;
     reverse(a.begin(), a.end());
-    cout << (a=b)? "YES" : "NO"; 
-    
+    cout << ((a==b)? "YES" : "NO");    
 }
