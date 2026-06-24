db3406ce212347e6893c2924508557791ffe35f7
atetoon <i.am.dhruvpandey@gmail.com>
2026-06-24 22:58:22 +0530

    add solution of porblem Presents

---
+++ Presents.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, gift, a[100];
+    cin >> n;
+    for(int i = 1; i<= n; i++) {
+        cin >> gift;
+        a[gift] = i;
+    }
+    for(int i = 1; i<=n; i++) {
+        cout << a[i] << " ";
+    }
+}
