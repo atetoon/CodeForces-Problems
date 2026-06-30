515960dee9424122d42eec4f21937ce28cf0908b
atetoon <i.am.dhruvpandey@gmail.com>
2026-06-30 21:30:09 +0530

    add demo contest file

---
+++ demo.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    int y = 100000000;
+
+    while(t--) {
+        int num;
+        cin >> num;
+
+         y*= num;        
+    }
+    
+
+    return 0;
+}
