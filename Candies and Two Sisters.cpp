1170eefda0c40930992233acf27d87d3de3cc654
atetoon <i.am.dhruvpandey@gmail.com>
2026-07-03 23:57:59 +0530

    add solution of problem Sum, Candies and Two Sisters

---
+++ Candies and Two Sisters.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n <= 2)
+            cout << 0 << '\n';
+        else
+            cout << (n - 1) / 2 << '\n';
+    }
+
+    return 0;
+}
