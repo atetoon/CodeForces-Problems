70ad19e32cc4fbfea8c8d4793cf1a9bd91772bc6
atetoon <i.am.dhruvpandey@gmail.com>
2026-07-06 23:19:53 +0530

    add solution of problem Sum of Round Numbers

---
+++ Sum of Round Numbers.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t,n;
+    cin >> t;
+
+    while (t--) {
+        cin >> n;
+
+        int d = 10;
+        vector<int> v;
+
+        while (n) {
+            if(n%d) {
+                v.push_back(n%d);
+                n = n - n%d;
+            }
+            d = d * 10;
+        }
+        cout << v.size() << endl;
+        for(auto x: v) {
+            cout << x << " ";
+        }
+    }
+}
