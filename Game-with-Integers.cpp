74f4e0fbcd3dfbfb0e45aee90702706fbd5f5549
atetoon <i.am.dhruvpandey@gmail.com>
2026-07-12 19:51:31 +0530

    add solution for Game with Integers problem

---
+++ Game-with-Integers.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    int n;
+
+    while (t--) {
+        cin >> n;
+        
+        cout << (n%3 ? "First\n" : "Second\n");
+    }
+}
