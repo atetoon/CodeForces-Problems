b785b7e6ddba6d817490ac11a394aa470022d18e
atetoon <i.am.dhruvpandey@gmail.com>
2026-07-20 18:24:11 +0530

    add solution for Design Tutorial problem

---
+++ Design-Tutorial.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int a;
+
+    if (n % 2 == 0)
+        cout << n - 4 << " " << 4;
+    else
+        cout << n - 9 << " " << 9;
+}
