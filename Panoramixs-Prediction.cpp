9ec4ca6b64e540a7f45e8dc1a27c3ad070eac01b
atetoon <i.am.dhruvpandey@gmail.com>
2026-07-23 17:42:22 +0530

    refactor: consolidate duplicate main functions in Panoramix's Prediction solution

--- Panoramixs-Prediction.cpp
+++ Panoramixs-Prediction.cpp
@@ -21,3 +21,25 @@ int main() {
         }
     }
 }
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    for(int i = a+1; ;i++) {
+        bool prime = true;
+
+        for(int j = 2; j * j <= i; j++) {
+            if(i%j == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if(prime) {
+            if(i == b) cout << "YES";
+            else cout << "NO";
+            break;
+        }
+    }
+}
