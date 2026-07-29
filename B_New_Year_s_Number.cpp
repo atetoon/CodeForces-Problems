186ff68301de0e00d1f50371105c6ea8c92c9a6c
atetoon <i.am.dhruvpandey@gmail.com>
2026-07-30 04:43:42 +0530

    add solution for B New Year’s Number problem

---
+++ B_New_Year_s_Number.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<ll>;
+
+void solve() {
+    ll n, x, y; cin>>n;
+
+    if (n / 2020 >= n % 2020)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+    }
+
+int main() {
+    int t; cin>>t;
+    while(t--) {
+        solve();
+    }
+}
