eb0aeb4e9688d3cd1e9d90daeb2bfe12d1120d5b
atetoon <i.am.dhruvpandey@gmail.com>
2026-08-03 21:15:48 +0530

    add solution for Comparison String problem

---
+++ B_Comparison_String.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int main() {
+   int t; cin>>t;
+
+   while(t--) {
+    int n; cin>>n;
+    string s; cin>>s;
+
+    int ans = 1, cnt = 1;
+    for(int i = 1;i<s.size();i++) {
+        if(s[i]!=s[i-1]) cnt = 1;
+        else cnt++;
+        ans = max(ans, cnt);
+    }
+    cout<<ans+1<<endl;
+   }
+}
